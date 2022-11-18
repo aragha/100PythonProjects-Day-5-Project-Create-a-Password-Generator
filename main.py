bdcd4c75979a1f22b95a58b34d8915fa5a442cb6
resplit <67502036+aragha@users.noreply.github.com>
2022-11-18 18:47:02 +0000

    Completed day 5 password generator project

--- main.py
+++ main.py
@@ -11,7 +11,37 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-
+# for x in range(0, 5):
+llist = []
+slist = []
+nlist = []
+for i in range(0, nr_letters):
+  pos = random.randint(0, len(letters)-1)
+  llist.append(letters[pos])
+for i in range(0, nr_symbols):
+  pos = random.randint(0, len(symbols)-1)
+  slist.append(symbols[pos])
+for i in range(0, nr_numbers):
+  pos = random.randint(0, len(numbers)-1)
+  nlist.append(numbers[pos])
+  # print('-------------------------')  
+  # print(llist)
+  # print(slist)
+  # print(nlist)
+  # print('-------------------------')  
+pwdstr = ''
+for i in llist:
+  pwdstr += i
+for i in slist:
+  pwdstr += i
+for i in nlist:
+  pwdstr += i
+print(pwdstr)
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+fulllist = llist + slist + nlist
+pwdstr = ''
+for i in range(0, len(fulllist)):
+  pos = random.randint(0, len(fulllist) - 1)
+  pwdstr += fulllist[pos]
+print(pwdstr)
